4d1f345023c3753f7513c01110f0d420f4627d22
MrAaaah <jules.minier@mraaaah.fr>
2013-01-31 22:01:39 -0500

    First version

---
+++ main.py
@@ -0,0 +1,47 @@
+# -*-coding:Utf-8 -*
+import itertools
+
+# parsing du fichier (on colle tout dans des listes)
+f = open('input.txt', 'r')
+
+nbGarments = int(f.readline())
+
+l = list()
+money = list()
+size = list()
+
+for garment in xrange(nbGarments):
+	(m, nbGClass) = f.readline().split()
+
+	l.append(list())
+
+	# on stock le nb d'argent
+	money.append(int(m))
+	size.append(int(nbGClass))
+
+	for gClass in xrange(int(nbGClass)):
+		models = f.readline().split()
+		
+		l[garment].append(list())
+
+		for i in xrange(int(models[0])):
+			l[garment][gClass].append(models[i + 1])
+
+
+# recherche d'achat optimal via bruteforce (test de toutes les valeurs une par une)
+for i in xrange(len(l)):
+	combs = list(itertools.product(*l[i])) # toutes les combinaison possible pour la catégorie 1
+	maxCost = 0
+
+	for comb in combs:
+		cost = 0
+		for elmt in comb:
+			cost += int(elmt)
+
+		if money[i] > cost > maxCost:
+			maxCost = cost
+
+	if maxCost == 0:
+		print "No solution"
+	else:
+		print maxCost
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Solution perso au problème : "Wedding Shopping"
+[Lien vers le problème](http://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=2445)
